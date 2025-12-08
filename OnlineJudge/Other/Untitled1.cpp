a0c5b69b42e19b2bdc915d5a83e27485d5c5d22e
axataris-cell <axataris@gmail.com>
2025-12-09 00:17:43 +0700

    Axataris's Code

--- OnlineJudge/Other/Untitled1.cpp
+++ OnlineJudge/Other/Untitled1.cpp
@@ -10,28 +10,54 @@ void file() {
 	freopen("", "w", stdout);
 }
 
+// Longest Common String / Segment
+int f[3001][3001] = {};
 
 void testcase() {
-    int n; cin >> n;
-    vector<int> a(n);
-    vector<int> res;
-	for (int i = 0; i < n; i++) cin >> A[i];
+	int n, m; cin >> n >> m;
+	vector<int> a(n), b(m);
+	for (int i = 0; i < n; i++) cin >> a[i];
+	for (int j = 0; j < m; j++) cin >> b[j];
 	
-	stact<int> st;
-	for (int i = 0; i < n; i++) {
-		
+	for (int i = 1; i <= a.size(); i++) {
+		for (int j = 1; j <= b.size(); j++) {
+			if (a[i - 1] == b[j - 1]) {
+				f[i][j] = f[i - 1][j - 1] + 1;
+			} else {
+				f[i][j] = max(f[i - 1][j], f[i][j - 1]);
+			}
+		}
 	}
-    cout << el;
+	
+	// Truy ngược lại tìm xâu con chung
+    vector<int> res;
+    int i = a.size(), j = b.size();
+
+    while (i > 0 && j > 0) {
+        if (a[i - 1] == b[j - 1]) {
+            res.push_back(a[i - 1]);
+            i--, j--;
+        } 
+        else if (f[i - 1][j] > f[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+    reverse(res.begin(), res.end());
+    
+    cout << res.size() << el;
+    for (auto x : res) cout << x << ' ';
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
 	//file();
 
-	ll t = 1; cin >> t;
+	ll t = 1; //cin >> t;
 	while (t--) testcase();
 
 	return 0;
