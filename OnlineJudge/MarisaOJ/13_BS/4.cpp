2b44b6fea84efc4c8fd78622cc11d828d27d4464
axataris <axataris@gmail.com>
2026-04-14 07:59:07 +0700

    14.04.2026 07.59

---
+++ OnlineJudge/MarisaOJ/13_BS/4.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#define umap unordered_map
+#define uset unordered_set
+#define pqueue priority_queue
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define fi first
+#define se second
+#define null nullptr
+#define pb push_back
+#define len length
+#define ll long long
+#define ld long double
+#define el '\n'
+
+// Author: Axataris
+// Created: 2026-04-13 09:21
+
+constexpr int INF = 2e9;
+constexpr ll LINF = 4e18;
+
+#define FILENAME "4"
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+
+#ifdef LOCAL
+   #define debug(x) cerr << x << '\n'
+#else
+   #define debug(x)
+#endif
+
+mt19937 mt(chrono::steady_clock::now().time_since_epoch().count());
+
+void file() {
+    if (FILE *f = fopen(FILENAME".INP", "r")) {
+        fclose(f);
+        freopen(FILENAME".INP", "r", stdin);
+        freopen(FILENAME".OUT", "w", stdout);
+    }
+}
+
+#define int long long
+
+void testcase() {
+    int n, k; cin >> n >> k;
+    vector<int> a(n + 1), f(n + 1, 0);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        f[i] = f[i - 1] + a[i];
+    }
+    for (int i = 1; i <= n; i++) {
+        cerr << f[i] << ' ';
+    }
+    cerr << el;
+
+    int res = 0;
+    for (int r = 1; r <= n; r++) {
+        if (f[r] - k < 0) continue;
+        int l = upper_bound(f.begin(), f.begin() + r + 1, f[r] - k) - f.begin() - 1;
+        cerr << "l: ";debug(l);
+        cerr << "r: ";debug(r);
+        debug("");
+        if (l <= r && l >= 0) res += l + 1;
+    }
+    cout << res;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    file();
+
+    int t = 1; //cin >> t;
+    while (t--) testcase();
+
+    return 0;
+}
