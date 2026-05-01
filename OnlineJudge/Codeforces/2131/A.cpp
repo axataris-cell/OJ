a40af0d079fcecc4dbff8b71edff4a73f5387dfe
axataris <axataris@gmail.com>
2026-05-01 13:47:27 +0700

    01.05.2026 13.47

---
+++ OnlineJudge/Codeforces/2131/A.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define umap unordered_map
+#define uset unordered_set
+#define pqueue priority_queue
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define fi first
+#define se second
+#define null nullptr
+#define pb push_back
+#define len length
+#define ll long long
+#define ld long double
+#define el '\n'
+
+// Author: Fua
+
+constexpr int INF = 2e9;
+constexpr ll LINF = 4e18;
+
+using namespace std;
+using pii = pair<int, int>;
+using pll = pair<long long, long long>;
+
+#ifdef LOCAL
+   #define debug(x) cerr << x << '\n'
+#else
+   #define debug(x)
+#endif
+
+mt19937 mt(chrono::steady_clock::now().time_since_epoch().count());
+
+void testcase() {
+    
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1; //cin >> t;
+    while (t--) testcase();
+
+    return 0;
+}
