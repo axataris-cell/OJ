db9fd10d4dedd7ec289dc871965ab0ad9d33fae5
axataris-cell <axataris@gmail.com>
2026-07-10 23:43:43 +0700

    10.07.2026 23.43

--- OnlineJudge/MarisaOJ/35_ST/19.cpp
+++ OnlineJudge/MarisaOJ/35_ST/19.cpp
@@ -14,7 +14,7 @@
 #define el '\n'
 
 // Author: Axataris
-// Created: 2026-07-09 21:14
+// Created: 2026-07-10 19:25
 
 constexpr int INF = 2e9;
 constexpr ll LINF = 4e18;
@@ -41,8 +41,83 @@ void file() {
     }
 }
 
+const int MAXN = 2e5 + 5;
+
+int n, q;
+
+vector<int> a(MAXN, 0);
+vector<int> ST(4 * MAXN, 0); // min
+
+struct Query {
+    int l, id;
+};
+vector<Query> queries[MAXN];
+
+void update(int id, int l, int r, int pos, int val) {
+    if (l == r) {
+        ST[id] = val;
+        return;
+    }
+    int mid = (l + r) / 2;
+    if (pos <= mid) update(id << 1, l, mid, pos, val);
+    else update(id << 1 | 1, mid + 1, r, pos, val);
+    ST[id] = min(ST[id << 1], ST[id << 1 | 1]);
+}
+
+int query(int id, int l, int r, int ql, int qr) {
+    if (r < ql || l > qr) return INF;
+    if (ql <= l && r <= qr) return ST[id];
+    int mid = (l + r) / 2;
+    return min(query(id << 1, l, mid, ql, qr), query(id << 1 | 1, mid + 1, r, ql, qr));
+}
+
+int find(int id, int l, int r, int ql, int qr, int L) {
+    if (r < ql || l > qr) return -1;
+
+    if (ql <= l && r <= qr) {
+        if (ST[id] >= L) return -1;
+        if (l == r) return l;
+    }
+
+    int mid = (l + r) / 2;
+
+    int res = find(id << 1, l, mid, ql, qr, L);
+    if (res != -1) return res;
+
+    return find(id << 1 | 1, mid + 1, r, ql, qr, L);
+}
+
+
 void testcase() {
-    
+    cin >> n >> q;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 1; i <= q; i++) {
+        int l, r; cin >> l >> r;
+        queries[r].pb({l, i});
+    }
+
+    vector<int> ans(q + 1, 0);
+    map<int, int> lastpos;
+    for (int r = 1; r <= n; r++) {
+        if (lastpos.count(a[r])) {
+            update(1, 1, n, r, lastpos[a[r]]);
+            update(1, 1, n, lastpos[a[r]], INF);
+        }
+        else {
+            update(1, 1, n, r, 0);
+        }
+        for (auto &[l, id] : queries[r]) {
+            int pos = find(1, 1, n, l, r, l);
+
+            if (pos == -1) ans[id] = 0;
+            else ans[id] = a[pos];
+        }
+        lastpos[a[r]] = r;
+    }
+
+    for (int i = 1; i <= q; i++) cout << ans[i] << el;
 }
 
 int32_t main() {
